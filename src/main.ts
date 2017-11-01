82d97c3742c119bce410fb9334986e00890c8529
Jérôme Schneider <contact@netgusto.com>
2017-11-01 15:56:07 +0100

    WIP

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,11 @@
 
 import comm from './comm';
 
+const debugpoints = [];
+
+const debugMaterial = new pc.PhongMaterial();
+debugMaterial.shadingModel = pc.SPECULAR_BLINN;
+
 export default async function main(app: any, wsurl: string, tps: number, cdnbaseurl: string) : Promise<any> {
 
     const agent = new window.pc.Entity();
@@ -11,6 +16,7 @@ export default async function main(app: any, wsurl: string, tps: number, cdnbase
     agent.isStatic = false;
     agent.castShadowsLightmap = false;
     agent.castShadows = true;
+    window.agent = agent;
 
     app.root.addChild(agent);
 
@@ -18,7 +24,23 @@ export default async function main(app: any, wsurl: string, tps: number, cdnbase
         //console.log("SETMAP", map, app);
     }, function(msg) {
         //console.log("VIZMSG", msg);
+
+        debugpoints.map(pEntity => pEntity.destroy());
+        debugpoints.length = 0;
+
+        msg.DebugPoints.map(point => {
+            const p = new window.pc.Entity();
+            p.addComponent("model", { type: "sphere" });
+            p.setPosition(point[0], 0.8, point[1]);
+            p.setLocalScale(0.1, 0.1, 0.1);
+            p.isStatic = true;
+            p.castShadowsLightmap = false;
+            p.castShadows = false;
+            app.root.addChild(p);
+            debugpoints.push(p);
+        });
+
         const pos = msg.Objects[0].Position;
-        agent.setPosition(pos[0], 0.8, pos[1]);
+        agent.setPosition(pos[0], 0.2, pos[1]);
     });
 }
