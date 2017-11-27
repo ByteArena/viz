4ddd484bcc09d3656e574812bda53c8f9e69c57d
Sven SAULEAU <sven.sauleau@xtuc.fr>
2017-11-27 22:34:18 +0100

    fix: remove unused resize events

--- src/app/actions/game.js
+++ src/app/actions/game.js
@@ -1,14 +1,5 @@
 // @flow
 
-export function resize(mode: string, width: number, height: number) {
-    return {
-        type: "RESIZE",
-        mode,
-        width,
-        height,
-    }
-}
-
 export function addFrameBatch(frames: Array<Vizmessage>) {
     return {
         type: "ADD_FRAME_BUCKET",
--- src/app/reducers/game.js
+++ src/app/reducers/game.js
@@ -8,13 +8,6 @@ const initialState: State = {};
 
 export function game(state: State = initialState, action: Object): State {
     switch (action.type) {
-        case "RESIZE":
-            return Object.assign({}, state, {
-                width: action.width,
-                height: action.height,
-                mode: action.mode,
-            });
-
         case "ADD_FRAME": {
             return Object.assign({}, state, {
                 frame: action.frame,
